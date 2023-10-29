cc631f57e61a81b2e34cdd4a012fc840b7bdf2c0
eeburgosz <hdst.bur@gmail.com>
2023-10-29 12:56:45 -0300

    GOOGLE_APPLICATION_CREDENTIALS

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,11 +1,14 @@
 // const firebase = require("firebase-admin");
 require('dotenv').config();
+const { GOOGLE_APPLICATION_CREDENTIALS } = process.env;
 
 const { initializeApp, applicationDefault } = require("firebase-admin/app");
+const firebase = require("firebase-admin");
 const { getFirestore } = require("firebase-admin/firestore");
 
+
 initializeApp({
-   credential: applicationDefault(),
+   credential: firebase.credential.cert(GOOGLE_APPLICATION_CREDENTIALS),
 });
 
 const firestoreDb = getFirestore();
